f253fa6d5f47a46a714aaaccdd78ded3c74e027a
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2021-12-04 21:46:01 +0330

    Update studentsingup.php

--- studentsingup.php
+++ studentsingup.php
@@ -1,27 +1,46 @@
-<?php
-$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-$bberry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
-$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
-$webos = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
-if ($android || $bberry || $iphone || $ipod || $webos== true) 
-{ 
-header('Location: http://localhost/study_hall/students/studentsingup.php/mobile');
-}
-?>
+</div>
 <link rel="stylesheet" href="/study_hall/students/singup.ui.css">
 <div class="login-wrap">
+
 	<div class="login-html">
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+	  <h2>سامانه ورود و ثبت نام</h2>
+
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">ورود</label>
+		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">ثبت نام</label>
 		<div class="login-form">
-			<div class="sign-in-htm">
+
+			<div class="sign-up-htm">
 				<div class="group">
-					<label for="user" class="label">Username</label>
+					<label for="user" class="label">نام</label>
 					<input id="user" type="text" class="input">
 				</div>
 				<div class="group">
-					<label for="pass" class="label">Password</label>
+					<label for="pass" class="label">نام خانوادگي</label>
+					<input id="pass" type="text" class="input" data-type="text">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">كلاس</label>
+					<input id="pass" type="text" class="input" data-type="text">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">شماره تماس</label>
+					<input id="pass" type="phon" class="input">
+				</div>
+				<div class="group">
+					<input type="submit" class="button" value="Sign Up">
+				</div>
+				<div class="hr"></div>
+				<div class="foot-lnk">
+					<label for="tab-1">اكنون عضو هستيد؟</a>
+				</div>
+			</div>
+						<div class="sign-in-htm">
+				<div class="group">
+					<label for="user" class="label">گذرواژه</label>
+					<input id="user" type="text" class="input">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">رمز ورود</label>
 					<input id="pass" type="password" class="input" data-type="password">
 				</div>
 				<div class="group">
@@ -36,31 +55,7 @@ header('Location: http://localhost/study_hall/students/studentsingup.php/mobile'
 					<a href="#forgot">Forgot Password?</a>
 				</div>
 			</div>
-			<div class="sign-up-htm">
-				<div class="group">
-					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input">
-				</div>
-				<div class="group">
-					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
-				</div>
-				<div class="group">
-					<label for="pass" class="label">Repeat Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
-				</div>
-				<div class="group">
-					<label for="pass" class="label">Email Address</label>
-					<input id="pass" type="text" class="input">
-				</div>
-				<div class="group">
-					<input type="submit" class="button" value="Sign Up">
-				</div>
-				<div class="hr"></div>
-				<div class="foot-lnk">
-					<label for="tab-1">Already Member?</a>
-				</div>
-			</div>
 		</div>
 	</div>
 </div>
+</html>
